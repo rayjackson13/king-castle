6498eded868904a6b5d013968652f5bce966d436
rayjackson13 <rayjackson@live.ru>
2022-12-11 08:33:44 +0300

    Adding file to git commit after modifiyng version.

--- scripts/amendCommit.cjs
+++ scripts/amendCommit.cjs
@@ -4,9 +4,7 @@ var filePath = 'src/app.json';
 
 exec('git diff --name-only', function(error, stdout) {
   var modifiedFiles = stdout.trim().split(/\r?\n/);
-  console.log(modifiedFiles, filePath);
   if (modifiedFiles.includes(filePath)) {
-    console.log('yes', `git add ${filePath}`);
     // amend last commit
     exec(`git commit --amend -C HEAD -n ${filePath}`);
   }
