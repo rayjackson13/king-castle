277599d4118485d3d22f7b2dee294853701bcc50
rayjackson13 <rayjackson@live.ru>
2022-12-11 08:11:45 +0300

    Updating version on commit.

--- scripts/amendCommit.cjs
+++ scripts/amendCommit.cjs
@@ -3,7 +3,11 @@ var exec = require('child_process').exec;
 
 var filePath = path.resolve(__dirname, '../src/app.json');
 
-exec('git diff --name-only', function(error, stdout, stderr) {
+exec('git diff --name-only', function(error, stdout) {
   var modifiedFiles = stdout.trim().split(/\r?\n/);
   console.log(modifiedFiles);
-})
+  if (modifiedFiles.includes(filePath)) {
+    // amend last commit
+    exec(`git commit --amend -C HEAD -n ${filePath}`);
+  }
+});
